ebbc5c78059ab78a2ec7ecf939f11a21da241ab3
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-02-06 20:38:50 -0800

    added updateSales function for adding sales to html file

--- mightygumball.js
+++ mightygumball.js
@@ -1,14 +1,27 @@
 window.onload = function() {
-	var url = "http://someserver.com/data.json";
+	var url = "http://gumball.wickedlysmart.com";
 	var request = new XMLHttpRequest();
 	request.open("GET", url);
 
 	request.onload = function() {
 		if (request.status == 200) {
-			alert(request.responseText);
+			updateSales(request.responseText);
 	};
-
+	
 	request.send(null);
 
 	};
+}
+
+function updateSales(responseText) {
+	var salesDiv = document.getElementById("sales");
+	var sales = JSON.parse(responseText);
+
+	for (var i = 0; i < sales.length; i++) {
+		var sale = sales[i];
+		var div = document.createELement("div");
+		div.setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+		salesDiv.appendChild(div);
+	}
 }
